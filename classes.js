25e39d5c83cd00005c45e4103bdb147d0426dec7
ralphvw <williamsralph@enyata.com>
2023-07-24 14:58:50 +0000

    Classes

---
+++ classes.js
@@ -0,0 +1,13 @@
+// class Student
+
+// constructor name: string, age:number, score:number, courses:[string]
+
+/**
+ * Methods
+ * name() = return name of the student
+ * age() = return the age
+ * increaseAge() = increase the students age by 1
+ * addCourse() = adds a course to courses
+ */
+
+
--- promise.js
+++ promise.js
@@ -1,16 +1,16 @@
 import axios from "axios";
 
 const fn = async () => {
-  axios.get(`https://api.github.com/users/milligosh`).then((data) => {
+  axios.get(`https://api.github.com/users/milligosh`).then(() => {
     console.log(data)
   })
-  console.log(".....");
+  console.log("Downloading.......");
 };
 
 async function asyncfn() {
+  console.log("Downloading......");
   const data = await axios.get(`https://api.github.com/users/milligosh`);
   console.log(data);
-  console.log('....');
 }
 
 asyncfn();
